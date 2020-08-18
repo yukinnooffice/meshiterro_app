ed9bc666910673515372bcb2f5b0227f6c10f80f
vagrant <vagrant@localhost.localdomain>
2020-08-18 08:53:33 +0000

    [finish] 5-3

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'post_images#index'
-  resources :post_images, only: [:new, :create, :index, :show]
+   resources :post_images, only: [:new, :create, :index, :show] do
+    resource :favorites, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
+  end
 end
